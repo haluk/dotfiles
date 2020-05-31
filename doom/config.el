352a57ddf4e968acc3963b9d3fa8bfd8596ac97d
haluk <hdogan@vivaldi.net>
2020-05-31 01:24:52 -0500

    update doom config

--- doom/config.el
+++ doom/config.el
@@ -1,48 +1,59 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
-;; UI and Keyboard
-;;(setq doom-font (font-spec :family "Fantasque Sans Mono" :size 16))
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Haluk Dogan"
+      user-mail-address "hdogan@vivaldi.net")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "JetBrains Mono" :size 16))
-;;(setq doom-font (font-spec :family "Hasklig" :size 14))
+;;(setq doom-font (font-spec :family "monospace" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-(move-text-default-bindings)
-(global-set-key (kbd "C-c d") 'multi-line)
 
-;; Python
-(setenv "WORKON_HOME" "/home/hd/.local/share/virtualenvs/")
+;;(setenv "WORKON_HOME" "/home/hd/.local/share/virtualenvs/")
+(setenv "WORKON_HOME" "~/.cache/pypoetry/virtualenvs/")
 (add-hook 'before-save-hook 'py-isort-before-save)
-
-(when (executable-find "ipython")
-  (setq python-shell-interpreter "ipython"))
-
-
-;; Latex
-(defun my-reftex-formats-hook ()
-  (setq reftex-cite-format
-    '((97 . "\\autocite[]{%l}")
-     (98 . "\\blockcquote[]{%l}{}")
-     (99 . "\\cite[]{%l}")
-     (102 . "\\footcite[]{%l}")
-     (110 . "\\nocite{%l}")
-     (112 . "\\parencite[]{%l}")
-     (115 . "\\smartcite[]{%l}")
-     (116 . "\\textcite[]{%l}")
-     (80 . "~\\citep[]{%l}"))))
-
-(add-hook 'reftex-mode-hook 'my-reftex-formats-hook)
-
-;; Other
-(setq langtool-language-tool-jar "/home/hd/bin/languagetool-commandline.jar")
-
-;; Misc
-(defun set-exec-path-from-shell-PATH ()
-  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-  This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
-  (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(set-exec-path-from-shell-PATH)
